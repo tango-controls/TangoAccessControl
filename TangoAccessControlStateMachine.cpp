e2708581834a74a72cfd9738df09f2019875eef9
pascal_verdier <Pascal-Verdier@users.noreply.github.com>
2013-03-08 12:31:49 +0000

    Update file headers

--- TangoAccessControlStateMachine.cpp
+++ TangoAccessControlStateMachine.cpp
@@ -4,13 +4,13 @@ static const char *RcsId = "$Id$";
 //
 // file :        TangoAccessControlStateMachine.cpp
 //
-// description : C++ source for the «name» and its alowed
+// description : C++ source for the ï¿½nameï¿½ and its alowed
 //               methods for commands and attributes
 //
 // project :     Tango Access Control Management.
 //
 //
-// Copyright (C) :      2004,2005,2006,2007,2008,2009,2010,2011,2012
+// Copyright (C) :      2004,2005,2006,2007,2008,2009,2010,2011,2012,2013
 //						European Synchrotron Radiation Facility
 //                      BP 220, Grenoble 38043
 //                      FRANCE
